9ecde4f471380f503654304a163ae7d8867b8482
cazanon <carlos.azanon@gmail.com>
2014-10-04 12:41:56 +0200

    Creadas clases de test

---
+++ src/test/java/es/upm/miw/spai/ecp2/DecimalCollectionTest.java
@@ -0,0 +1,30 @@
+package es.upm.miw.spai.ecp2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DecimalCollectionTest {
+
+    private Point pt;
+
+    @Before
+    public void before() {
+        pt = new Point(2, 3);
+    }
+
+    @Test
+    public void testPuntoIntInt() {
+        assertEquals(2, pt.getX());
+        assertEquals(3, pt.getY());
+    }
+
+    @Test
+    public void testPunto() {
+        pt = new Point();
+        assertEquals(0, pt.getX());
+        assertEquals(0, pt.getY());
+    }
+    
+}
